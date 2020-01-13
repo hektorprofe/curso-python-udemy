22e1c826baf3fa56c5d58d728086ba163af3c472
overdrive80 <lukillas.ole@gmail.com>
2020-01-13 04:24:28 +0100

    Add files via upload

---
+++ Fase 4 - Temas avanzados/Tema 16 - Documentacion y pruebas/Apuntes/mi_modulo.py
@@ -0,0 +1,9 @@
+"""Este es el docstring del módulo"""
+
+def despedir():
+    """Este es el docstring de la función despedir"""
+    print("Adiós! Me estoy despidiendo desde la función despedir() del módulo")
+
+def saludar():
+    """Este es el docstring de la función saludar"""
+    print("Hola! Te estoy saludando desde la función saludar() del módulo")
